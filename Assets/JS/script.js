3a55e34877b35d2a7ec093902f2e89609822b3d1
ymasog <yer.masog@gmail.com>
2021-01-14 18:29:55 -0600

    added onclick event for submit button

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,12 @@
 $(function () {
     // console.log("ready!");
-
-    $("#submitBtn").on("click", function() {
-        cityName = 
-    })
-
-    // $(this).attr("data-name");
     var cityName = "minneapolis"
     var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=18687cf1c7c1d78e93e8472d225dee33&units=imperial"
 
+    $("#submitBtn").on("click", function () {
+        cityName = $("#submitBtn").val()
+        // console.log(cityName)
+    })
 
     $.ajax({
         url: queryURL,
@@ -41,14 +39,14 @@ $(function () {
             // console.log(dailyicon)
 
             var dailytemp = boogers.daily[0].temp.day
-            $("#todaytemp").text("Temp: " + dailytemp + " °F")  
+            $("#todaytemp").text("Temp: " + dailytemp + " °F")
 
             var dailyhumid = boogers.daily[0].humidity
             $("#todayhumid").text("Humidity: " + dailyhumid + " %")
 
 
-             
-            
+
+
         });
     });
 
